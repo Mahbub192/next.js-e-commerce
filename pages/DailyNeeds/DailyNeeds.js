1bdfb5e31f9d734b910ad6b47fadea8c0a136c7e
unknown <mahabubaligub192@gmail.com>
2023-11-18 18:01:03 +0600

    change card section

--- pages/DailyNeeds/DailyNeeds.js
+++ pages/DailyNeeds/DailyNeeds.js
@@ -3,14 +3,17 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { AuthContext } from "../providers/AuthProvider";
+import Link from "next/link";
 
 const DailyNeeds = () => {
   const {
     state: { products, error, loading },
   } = useContext(AuthContext);
-  const dailyProduct = products?.filter(
+  const healthBeauty = products?.filter(
     (singleProduct) => singleProduct.category == "Health & Beauty"
-
+  );
+  const dailyNeed = products?.filter(
+    (singleProduct) => singleProduct.category == "Daily Needs"
   );
   console.log(8, products);
   const settings = {
@@ -25,21 +28,62 @@ const DailyNeeds = () => {
   };
 
   return (
-    <div className="container mx-auto bg-gray-300 p-10 mb-10">
-      <div>
-        <h1 className="text-xl font-bold underline italic font-serif">Health & Beauty</h1>
-      </div>
-      <div>
-        <Slider {...settings}>
-          {dailyProduct?.map((singleProduct) => (
-            <div className="mt-5">
-              <div>
-                <img className="bg-white h-36 w-36 object-cover" src={singleProduct?.images[0]} alt="" />
-                <p className="mt-3"><span className="italic pr-3">Brand:</span>{singleProduct?.brand}</p>
+    <div>
+      <div className="container mx-auto bg-gray-300 p-10 mb-10 rounded-xl">
+        <div>
+          <h1 className="text-xl font-bold underline italic font-serif">
+            Health & Beauty
+          </h1>
+        </div>
+        <div>
+          <Slider {...settings}>
+            {healthBeauty?.map((singleProduct) => (
+              <div className="mt-5">
+                <Link href={`/Products/${singleProduct?._id}`}>
+                  <div>
+                    <img
+                      className="bg-white h-36 w-36 object-cover"
+                      src={singleProduct?.images[0]}
+                      alt=""
+                    />
+                    <p className="mt-3">
+                      <span className="italic pr-3">Brand:</span>
+                      {singleProduct?.brand}
+                    </p>
+                  </div>
+                </Link>
               </div>
-            </div>
-          ))}
-        </Slider>
+            ))}
+          </Slider>
+        </div>
+      </div>
+      <div className="container mx-auto bg-gray-300 p-10 mb-10 rounded-xl">
+        <div>
+          <h1 className="text-xl font-bold underline italic font-serif">
+          Daily Needs
+          </h1>
+        </div>
+        <div>
+          <Slider {...settings}>
+            {dailyNeed?.map((singleProduct) => (
+              <div className="mt-5">
+                <Link href={`/Products/${singleProduct?._id}`}>
+                  <div>
+                    <img
+                      className="bg-white h-36 w-36 object-cover"
+                      src={singleProduct?.images[0]}
+                      alt=""
+                    />
+                    <p className="mt-3">
+                      <span className="italic pr-3">Brand:</span>
+                      {singleProduct?.brand}
+                    </p>
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </Slider>
+        </div>
       </div>
     </div>
   );
